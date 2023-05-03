ff2ce7438d684e3cf24895a2fb703d581599b7c5
dannybonz <115420954+dannybonz@users.noreply.github.com>
2023-05-03 12:53:45 +0100

    Removed temporary debug text

--- set_winner.php
+++ set_winner.php
@@ -5,8 +5,7 @@
 
 	//Users must be logged in as an admin or business account in order to manage an event
 	if ((!isset($_SESSION["accountDetails"])) or (!($_SESSION["accountDetails"]["accountType"]=="Business" or $_SESSION["accountDetails"]["accountType"]=="Admin"))) {
-//		header ("Location: error.php?e=1"); //If not logged in as a business or admin account, redirect to error page
-		echo "oh...";	
+		header ("Location: error.php?e=1"); //If not logged in as a business or admin account, redirect to error page
 		exit(); //Cease execution of this page
 	} else {
 		include 'database.php'; //If we are logged in, then load the database
@@ -15,8 +14,7 @@
 		if ($count) { //If a match is found (i.e. there is a competition with the given ID
 			$row=mysqli_fetch_assoc($result); //Turn event data into array
 			if (!($row["accountID"]==$_SESSION["accountDetails"]["accountID"] or $_SESSION["accountDetails"]["accountType"]=="Admin")) { //If not logged in as the same user who created the event, or an admin account
-//				header("Location: error.php?e=1"); //Redirect to error page
-				echo "yeah...";
+				header("Location: error.php?e=1"); //Redirect to error page
 				exit();
 			} else {
 				$winnerID = uniqid();
